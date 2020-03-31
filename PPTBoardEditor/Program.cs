423836900da3eba3092c19e6a115da8bc8324809
FugEbinMeme <47312792+FugEbinMeme@users.noreply.github.com>
2020-03-31 01:36:26 -0700

    Pepega way of making it 1 window only
    I'm not a programmer

--- PPTBoardEditor/Program.cs
+++ PPTBoardEditor/Program.cs
@@ -5,17 +5,15 @@ using System.Windows.Forms;
 
 namespace PPTBoardEditor {
     static class Program {
-        static PlayerForm[] players = new PlayerForm[2];
+        static PlayerForm[] players = new PlayerForm[1];
 
         [STAThread]
         static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            for (int i = 0; i < 2; i++) {
-                players[i] = new PlayerForm(i);
-                players[i].Show();
-            }
+            players[0] = new PlayerForm(0);
+            players[0].Show();
 
             Application.Run();
         }
